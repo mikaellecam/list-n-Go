6bb38684ab979ffbc840a2eb0d963b451c09d699
mikaellecam <lecam.mikael@gmail.com>
2025-03-25 15:49:59 +0100

    created screen placeholders and completed router

--- lib/core/router.dart
+++ lib/core/router.dart
@@ -1,8 +1,35 @@
 import 'package:go_router/go_router.dart';
 
+import '../views/screens/create_product_screen.dart';
 import '../views/screens/home_screen.dart';
+import '../views/screens/login_screen.dart';
+import '../views/screens/product_list_screen.dart';
+import '../views/screens/product_screen.dart';
+import '../views/screens/profile_screen.dart';
+import '../views/screens/purchase_history_list_screen.dart';
+import '../views/screens/purchase_history_screen.dart';
+import '../views/screens/search_screen.dart';
 
 final GoRouter router = GoRouter(
   initialLocation: '/home',
-  routes: [GoRoute(path: '/home', builder: (context, state) => HomeScreen())],
+  routes: [
+    GoRoute(path: '/login', builder: (context, state) => LoginScreen()),
+    GoRoute(path: '/home', builder: (context, state) => HomeScreen()),
+    GoRoute(path: '/list', builder: (context, state) => ProductListScreen()),
+    GoRoute(path: '/product', builder: (context, state) => ProductScreen()),
+    GoRoute(path: '/search', builder: (context, state) => SearchScreen()),
+    GoRoute(
+      path: '/create-product',
+      builder: (context, state) => CreateProductScreen(),
+    ),
+    GoRoute(path: '/profile', builder: (context, state) => ProfileScreen()),
+    GoRoute(
+      path: '/purchase-history-list',
+      builder: (context, state) => PurchaseHistoryListScreen(),
+    ),
+    GoRoute(
+      path: '/purchase-history',
+      builder: (context, state) => PurchaseHistoryScreen(),
+    ),
+  ],
 );
---
+++ lib/views/screens/purchase_history_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class PurchaseHistoryScreen extends StatelessWidget {
+  const PurchaseHistoryScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
