dc12b8a8181a9616ce1b516c9e9e16c42fa57697
mikaellecam <lecam.mikael@gmail.com>
2025-03-27 18:53:44 +0100

    fixed date interactions

--- lib/models/product_list/product_list.dart
+++ lib/models/product_list/product_list.dart
@@ -3,16 +3,28 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'product_list.freezed.dart';
 part 'product_list.g.dart';
 
+DateTime _defaultDateTimeNow() => DateTime.now();
+
 @freezed
 abstract class ProductList with _$ProductList {
   const factory ProductList({
     int? id,
     required int userId,
     required String name,
-    DateTime? createdAt,
-    DateTime? updatedAt,
+    required DateTime createdAt,
+    required DateTime updatedAt,
   }) = _ProductList;
 
   factory ProductList.fromJson(Map<String, dynamic> json) =>
       _$ProductListFromJson(json);
+
+  factory ProductList.createNew({required int userId, required String name}) {
+    final now = DateTime.now();
+    return ProductList(
+      userId: userId,
+      name: name,
+      createdAt: now,
+      updatedAt: now,
+    );
+  }
 }
