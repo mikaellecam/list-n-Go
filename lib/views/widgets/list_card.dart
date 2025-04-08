f11f10936cc6599b6206a3ca34daedf720fb0f11
mikaellecam <lecam.mikael@gmail.com>
2025-04-08 23:21:58 +0200

    created custom app bar

--- lib/views/widgets/list_card.dart
+++ lib/views/widgets/list_card.dart
@@ -58,7 +58,10 @@ class _ListCardState extends State<ListCard> {
       child: Padding(
         padding: const EdgeInsets.all(8.0),
         child: InkWell(
-          onTap: () => context.push('/product'),
+          onTap: () {
+            _productListService.currentList.value = widget.productList;
+            context.push('/list');
+          },
           splashColor: Colors.transparent,
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
