ac5c31095deed4daf8f65b0e8e8560eb4b9e0e06
mikaellecam <lecam.mikael@gmail.com>
2025-04-08 22:56:43 +0200

    removed expanded widget

--- lib/views/widgets/list_card.dart
+++ lib/views/widgets/list_card.dart
@@ -16,12 +16,14 @@ class ListCard extends StatefulWidget {
 
 class _ListCardState extends State<ListCard> {
   late final TextEditingController _controller;
+  late final ProductListService _productListService;
   bool _isRenaming = false;
 
   @override
   void initState() {
     super.initState();
     _controller = TextEditingController(text: widget.productList.name);
+    _productListService = getIt<ProductListService>();
   }
 
   @override
@@ -41,7 +43,11 @@ class _ListCardState extends State<ListCard> {
       _isRenaming = false;
     });
     String newName = _controller.text;
-    // TODO Rename the list
+    if (newName.isEmpty) {
+      return;
+    }
+    widget.productList.name = newName;
+    _productListService.updateList(widget.productList);
   }
 
   @override
@@ -51,123 +57,120 @@ class _ListCardState extends State<ListCard> {
       margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
       child: Padding(
         padding: const EdgeInsets.all(8.0),
-        child: Expanded(
-          child: InkWell(
-            onTap: () => context.push('/product'),
-            splashColor: Colors.transparent,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Expanded(
-                  child: Row(
-                    children: [
-                      Image.asset(
-                        'assets/app_assets/basket_picture.png',
-                        width: 55,
-                        height: 55,
-                      ),
-                      const SizedBox(width: 20),
-                      Expanded(
-                        // Add this Expanded widget
-                        child:
-                            _isRenaming
-                                ? TextField(
-                                  controller: _controller,
-                                  autofocus: true,
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w700,
-                                    fontFamily: "Lato",
-                                    color: Colors.black,
-                                  ),
-                                  decoration: const InputDecoration(
-                                    border: InputBorder.none,
-                                    focusedBorder: InputBorder.none,
-                                    isDense:
-                                        true, // Makes the field more compact
-                                    contentPadding:
-                                        EdgeInsets
-                                            .zero, // Removes internal padding
-                                  ),
-                                )
-                                : Text(
-                                  widget.productList.name,
-                                  softWrap: true,
-                                  overflow: TextOverflow.ellipsis,
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.w700,
-                                    fontFamily: "Lato",
-                                    color: Colors.black,
-                                  ),
+        child: InkWell(
+          onTap: () => context.push('/product'),
+          splashColor: Colors.transparent,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/app_assets/basket_picture.png',
+                      width: 55,
+                      height: 55,
+                    ),
+                    const SizedBox(width: 20),
+                    Expanded(
+                      // Add this Expanded widget
+                      child:
+                          _isRenaming
+                              ? TextField(
+                                controller: _controller,
+                                autofocus: true,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Lato",
+                                  color: Colors.black,
                                 ),
+                                decoration: const InputDecoration(
+                                  border: InputBorder.none,
+                                  focusedBorder: InputBorder.none,
+                                  isDense: true, // Makes the field more compact
+                                  contentPadding:
+                                      EdgeInsets
+                                          .zero, // Removes internal padding
+                                ),
+                              )
+                              : Text(
+                                widget.productList.name,
+                                softWrap: true,
+                                overflow: TextOverflow.ellipsis,
+                                style: const TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.w700,
+                                  fontFamily: "Lato",
+                                  color: Colors.black,
+                                ),
+                              ),
+                    ),
+                  ],
+                ),
+              ),
+              if (_isRenaming)
+                IconButton(
+                  onPressed: () {
+                    FocusScope.of(context).unfocus();
+                    finishRenaming();
+                  },
+                  icon: const Icon(
+                    Icons.check,
+                    color: Colors.orange,
+                  ), // Changed to orange to match menu color
+                ),
+              PopupMenuButton(
+                iconColor: Colors.orange,
+                color: Colors.white,
+                itemBuilder:
+                    (context) => [
+                      PopupMenuItem(
+                        onTap: startRenaming,
+                        child: const Text(
+                          'Rename',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                      PopupMenuItem(
+                        child: const Text(
+                          'Remove',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        onTap: () async {
+                          bool? confirm = await showDialog(
+                            context: context,
+                            builder:
+                                (context) => AlertDialog(
+                                  title: const Text('Confirmation'),
+                                  content: const Text(
+                                    'Êtes-vous sûr de vouloir supprimer cet élément ?',
+                                  ),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () => context.pop(false),
+                                      child: const Text('Annuler'),
+                                    ),
+                                    TextButton(
+                                      onPressed: () => context.pop(true),
+                                      child: const Text('Supprimer'),
+                                    ),
+                                  ],
+                                ),
+                          );
+                          if (confirm != null && confirm) {
+                            await getIt<ProductListService>().removeList(
+                              widget.productList,
+                            );
+                            if (context.mounted) {
+                              context.pop();
+                            }
+                          }
+                        },
                       ),
                     ],
-                  ),
-                ),
-                if (_isRenaming)
-                  IconButton(
-                    onPressed: () {
-                      FocusScope.of(context).unfocus();
-                      finishRenaming();
-                    },
-                    icon: const Icon(
-                      Icons.check,
-                      color: Colors.orange,
-                    ), // Changed to orange to match menu color
-                  ),
-                PopupMenuButton(
-                  iconColor: Colors.orange,
-                  color: Colors.white,
-                  itemBuilder:
-                      (context) => [
-                        PopupMenuItem(
-                          onTap: startRenaming,
-                          child: const Text(
-                            'Rename',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                        ),
-                        PopupMenuItem(
-                          child: const Text(
-                            'Remove',
-                            style: TextStyle(color: Colors.black),
-                          ),
-                          onTap: () async {
-                            bool? confirm = await showDialog(
-                              context: context,
-                              builder:
-                                  (context) => AlertDialog(
-                                    title: const Text('Confirmation'),
-                                    content: const Text(
-                                      'Êtes-vous sûr de vouloir supprimer cet élément ?',
-                                    ),
-                                    actions: <Widget>[
-                                      TextButton(
-                                        onPressed: () => context.pop(false),
-                                        child: const Text('Annuler'),
-                                      ),
-                                      TextButton(
-                                        onPressed: () => context.pop(true),
-                                        child: const Text('Supprimer'),
-                                      ),
-                                    ],
-                                  ),
-                            );
-                            if (confirm != null && confirm) {
-                              await getIt<ProductListService>().removeList(
-                                widget.productList,
-                              );
-                              if (context.mounted) {
-                                context.pop();
-                              }
-                            }
-                          },
-                        ),
-                      ],
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
